6a4827d6bf614c353ce31c3bf8d6efa3fc24f4db
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2025-09-02 20:48:59 +0000

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     # Render will set $PORT, fallback to 8000 locally
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
-        "main:app",       # filename:app_instance
+        "app:app",       # filename:app_instance
         host="0.0.0.0",
         port=port,
         reload=True       # enable reload locally for dev
