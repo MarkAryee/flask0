e5262f21f02c0fcd25d283567fb13eea2d9f8ae2
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2025-09-02 21:58:15 +0000

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -47,39 +47,13 @@ def delete_task(index):
 
 
 
-import tensorflow as tf
-from pytorch_tabnet.tab_model import TabNetClassifier
-import joblib
+# -------------------- API Route --------------------
+@app.post("/parse_speech", methods=['POST'])
+async def parse_speech(request: Request):
+    from helpers_nlp import parse_speech
+    return await parse_speech(request)
 
-#from flask_cors import CORS
-
-
-# Load model and scaler
-#model = joblib.load('random_forest_model.joblib')
-#scaler = joblib.load('scaler.joblib')
-
-model = tf.keras.models.load_model('ncd_models/hypertension/deep-model1.keras')
-scaler = joblib.load('ncd_models/hypertension/deep-scaler.joblib')
-csv_name = "ncd_models/hypertension/c2.csv"
-
-model2 = tf.keras.models.load_model('ncd_models/arthritis/deep-model1.keras')
-scaler2 = joblib.load('ncd_models/arthritis/deep-scaler.joblib')
-csv_name2 = "ncd_models/arthritis/selected.csv"
-
-model3 = tf.keras.models.load_model('ncd_models/lung_cancer/deep-model1.keras')
-scaler3 = joblib.load('ncd_models/lung_cancer/deep-scaler.joblib')
-csv_name3 = "ncd_models/lung_cancer/selected.csv"
-
-model4 = TabNetClassifier()
-model4.load_model('ncd_models/asthma/tabnet_asthma_model.zip')
-scaler4 = joblib.load('ncd_models/asthma/tabnet_scaler.joblib')
-csv_name4 = "ncd_models/asthma/selected.csv"
-
-model5 = TabNetClassifier()
-model5.load_model('ncd_models/diabetes/tabnet-model1.keras.zip')
-#model5 = tf.keras.models.load_model('ncd_models/diabetes/deep-model1.keras')
-scaler5 = joblib.load('ncd_models/diabetes/deep-scaler.joblib')
-csv_name5 = "ncd_models/diabetes/selected.csv"
+   
 
 
 
